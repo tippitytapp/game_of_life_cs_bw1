6fe11c3c99a00fc0819ced8ac5c2565d5d96560b
Marc Tapp <marc.tapp@gmail.com>
2020-06-21 14:35:03 -0400

    got it working, still needs styling

--- gameoflife/src/App.js
+++ gameoflife/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from "./components/Header"
 import Rules from "./components/Rules"
 import Grid from "./components/Grid"
+
 import Footer from "./components/Footer"
 import './App.css';
 
